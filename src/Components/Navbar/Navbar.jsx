4dec1e26b619c1dc5863a8043020af9ef13ad06c
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2022-01-16 16:05:03 +0530

    navbar changed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,13 +4,117 @@ import image from '../../assets/vitrendz-logo-new.svg';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <a className="navbar-brand" href="https://vitrendz.com/">
-                    <img src={image} alt="img" />
+                <a className="navbar-brand" href="/">
+                    Navbar
                 </a>
                 <button
                     className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                >
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a
+                                className="nav-link active"
+                                aria-current="page"
+                                href="/"
+                            >
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Link
+                            </a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a
+                                className="nav-link dropdown-toggle"
+                                href="/"
+                                id="navbarDropdown"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                Dropdown
+                            </a>
+                            <ul
+                                className="dropdown-menu"
+                                aria-labelledby="navbarDropdown"
+                            >
+                                <li>
+                                    <a className="dropdown-item" href="/">
+                                        Action
+                                    </a>
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="/">
+                                        Another action
+                                    </a>
+                                </li>
+                                <li>
+                                    <hr className="dropdown-divider" />
+                                </li>
+                                <li>
+                                    <a className="dropdown-item" href="/">
+                                        Something else here
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <a
+                                className="nav-link disabled"
+                                href="/"
+                                tabindex="-1"
+                                aria-disabled="true"
+                            >
+                                Disabled
+                            </a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            aria-label="Search"
+                        />
+                        <button
+                            className="btn btn-outline-success"
+                            type="submit"
+                        >
+                            Search
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
+
+{
+    /* <nav classNameName="navbar navbar-expand-lg navbar-light">
+            <div classNameName="container-fluid">
+                <a classNameName="navbar-brand" href="https://vitrendz.com/">
+                    <img src={image} alt="img" />
+                </a>
+                <button
+                    classNameName="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
                     data-bs-target="#navbarNav"
@@ -18,30 +122,30 @@ function Navbar() {
                     aria-expanded="false"
                     aria-label="Toggle navigation"
                 >
-                    <span className="navbar-toggler-icon"></span>
+                    <span classNameName="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item">
+                <div classNameName="collapse navbar-collapse" id="navbarNav">
+                    <ul classNameName="navbar-nav ms-auto">
+                        <li classNameName="nav-item">
                             <a
-                                className="nav-link"
+                                classNameName="nav-link"
                                 href="https://vitrendz.com/"
                             >
                                 About us
                             </a>
                         </li>
-                        <li className="nav-item">
+                        <li classNameName="nav-item">
                             <a
-                                className="nav-link"
+                                classNameName="nav-link"
                                 href="https://vitrendz.com/"
                             >
                                 Services
                             </a>
                         </li>
 
-                        <li className="nav-item">
+                        <li classNameName="nav-item">
                             <a
-                                className="nav-link"
+                                classNameName="nav-link"
                                 aria-current="page"
                                 href="https://community.vitrendz.com/"
                             >
@@ -51,8 +155,5 @@ function Navbar() {
                     </ul>
                 </div>
             </div>
-        </nav>
-    );
+        </nav> */
 }
-
-export default Navbar;
