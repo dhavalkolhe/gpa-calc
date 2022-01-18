ea0304a2efa2261954f1d8d8e5d1a5c147094490
Abhishek Agarwal <abhishek.agarwal.work20@gmail.com>
2022-01-18 14:08:17 +0530

    bug fix

--- src/Components/Calculators/Calculator.jsx
+++ src/Components/Calculators/Calculator.jsx
@@ -5,6 +5,8 @@ import peopleCry from "../../assets/people-crying.jpg";
 import mithaiBaat from "../../assets/mithai-baat-dijiye.jpg";
 import youCanDoIt from "../../assets/you-can-do-it.jpg";
 import disappointMeme from "../../assets/dis.jpg";
+import soFunnyKim from '../../assets/so-funny-kim.jpg';
+
 
 const Calculator = () => {
   // meme image link state
@@ -14,19 +16,42 @@ const Calculator = () => {
   const { register, handleSubmit } = useForm();
   let [total, setTotal] = useState(0);
 
+  function isEmpty(obj) {
+      let countEmptyFields = 0;
+      for (const [, value] of Object.entries(obj)) {
+          if (value === '') countEmptyFields++;
+      }
+      return countEmptyFields > 0;
+  }
+  let isFormDataEmpty;
+
   let onSubmit = (data) => {
-    let credits = 0;
-    let gpa = 0;
-    for (i = 0; i < sub; i++) {
-      credits += parseFloat(data[`credits-${i}`]);
-      gpa += parseFloat(data[`credits-${i}`] * data[`grades-${i}`]);
+    isFormDataEmpty = isEmpty(data);
+    if (!isFormDataEmpty) {
+        let credits = 0;
+        let gpa = 0;
+        for (i = 0; i < sub; i++) {
+            credits += parseFloat(data[`credits-${i}`]);
+            gpa += parseFloat(data[`credits-${i}`] * data[`grades-${i}`]);
+        }
+        setTotal((prevTotal) => (gpa / credits).toFixed(2));
+    } else {
+        setTotal(
+            (prevTotal) =>
+                'Please fill in all the fields or select only required number of semesters'
+        );
     }
-    setTotal((prevTotal) => (gpa / credits).toFixed(2));
+    
   };
 
   useEffect(() => {
     const memeHandler = () => {
-      if (total >= 9.5) {
+      if (
+          total ===
+          'Please fill in all the fields or select only required number of semesters'
+      ) {
+          setMemeImage((prevMeme) => soFunnyKim);
+      }else if (total >= 9.5) {
         setMemeImage((prevMeme) => peopleCry);
       } else if (total >= 9.0 && total < 9.5) {
         setMemeImage((prevMeme) => mithaiBaat);
@@ -46,7 +71,8 @@ const Calculator = () => {
           <br />
           <div className="subject-select-container">
             <select name="credits" {...register(`credits-${i}`)}>
-              c<option value="1">1</option>
+              <option value="" defaultValue>Credits</option>
+              <option value="1">1</option>
               <option value="1.5">1.5</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -54,7 +80,7 @@ const Calculator = () => {
               <option value="5">5</option>
             </select>
             <select name="grades" {...register(`grades-${i}`)}>
-              <option value="0" defaultValue>
+              <option value="" defaultValue>
                 Grade
               </option>
               <option value="10">S</option>
@@ -71,48 +97,50 @@ const Calculator = () => {
       );
     }
     return (
-      <div className="Calculator">
-        <div className="calculator-info">
-          <p className="steps">
-            <br />
-            <span>How to use GPA calculator</span> <br />
-            🔢 Enter number of courses <br />
-            🧐 Put grades obtained and credits of each course <br />
-            💥 Voila! Your GPA for this semester <br /> <br />
-          </p>
-        </div>
-        <div className="calculator-main">
-          <form className="count-form">
-            <h6>Subject Count: </h6>
-            <input
-              type="number"
-              onChange={(e) => setSub(e.target.value)}
-              className="count-input"
-            />
-          </form>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div>{list1}</div>
-            <br />
-            <br />
-            {sub > 0 && (
-              <button type="Submit" className="calc-btn">
-                Calculate
-              </button>
-            )}
-          </form>
-        </div>
-        <br />
-        <div className="calc-output">
-          {total !== 0 && (
-            <div>
-              <h1 className="answer">GPA: {total}</h1>
-              <div className="meme-image">
-                <img src={memeImage} alt="img" />
-              </div>
+        <div className="Calculator">
+            <div className="calculator-info">
+                <p className="steps">
+                    <br />
+                    <span>How to use GPA calculator</span> <br />
+                    🔢 Enter number of courses <br />
+                    🧐 Put grades obtained and credits of each course <br />
+                    💥 Voila! Your GPA for this semester <br /> <br />
+                </p>
             </div>
-          )}
+            <div className="calculator-main">
+                <form className="count-form">
+                    <h6>Subject Count: </h6>
+                    <input
+                        type="number"
+                        onChange={(e) => setSub(e.target.value)}
+                        className="count-input"
+                    />
+                </form>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>{list1}</div>
+                    <br />
+                    <br />
+                    {sub > 0 && (
+                        <button type="Submit" className="calc-btn">
+                            Calculate
+                        </button>
+                    )}
+                </form>
+            </div>
+            <br />
+            <div className="calc-output">
+                {total !== 0 && (
+                    <div>
+                        <h1 className="answer">
+                            {total.length > 5 ? `${total}` : `GPA: ${total}`}
+                        </h1>
+                        <div className="meme-image">
+                            <img src={memeImage} alt="img" />
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
-      </div>
     );
   } else {
     return (
