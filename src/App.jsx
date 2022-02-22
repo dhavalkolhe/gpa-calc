98f30ee2dc6390e013587be65e04481c851d7076
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2022-02-22 15:36:17 +0530

    ads added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
-import About from './Components/About/About';
-import Box from './Components/Box/Box';
-import Footer from './Components/Footer/Footer';
-import HeaderTwo from './Components/HeaderTwo/HeaderTwo';
-import './App.css';
-import Developers from './Components/Developers/Developers';
+import React from "react";
+import About from "./Components/About/About";
+import Box from "./Components/Box/Box";
+import Footer from "./Components/Footer/Footer";
+import HeaderTwo from "./Components/HeaderTwo/HeaderTwo";
+import "./App.css";
+import Developers from "./Components/Developers/Developers";
+import AdSense from 'react-adsense';
 
 function App() {
-    return (
-        <div className="landing">
-            <HeaderTwo />
-            <Box />
-            <About />
-            {/* <Developers /> */}
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="landing">
+      <HeaderTwo />
+      <Box />
+      <AdSense.Google
+        client="ca-pub-3524193275174890"
+        slot="4162029724"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        format="auto"
+        responsive="true"
+        // layoutKey="-gw-1+2a-9x+5c"
+      />
+      <About />
+      {/* <Developers /> */}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
